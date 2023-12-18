0cadc160cb06a15afbaa9daa3ec6a865b95a7c22
shwetd19 <shwetas.dhake@mitaoe.ac.in>
2023-12-18 21:40:14 +0530

    index updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -12,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 const port = 5000;
 
 // Hardcoded values
-const MongoURI = "mongodb+srv://shwetasdhake16:shwetasdhake16@cluster0.c9dylvi.mongodb.net/ML_Token?retryWrites=true&w=majority";
+const MongoURI =
+  "mongodb+srv://shwetasdhake16:shwetasdhake16@cluster0.c9dylvi.mongodb.net/ML_Token?retryWrites=true&w=majority";
 
 // Connect to MongoDB
 // Connect to MongoDB
@@ -23,11 +23,10 @@ const connect = () => {
       console.log("Connected to Database successfully on " + MongoURI);
     })
     .catch((err) => {
-      console.error('Error connecting to MongoDB:', err);
+      console.error("Error connecting to MongoDB:", err);
       process.exit(1); // Exit the process on connection failure
     });
 };
-
 
 // Routes
 const Route = require("./routes/token-route.js");
